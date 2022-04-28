a03c4798aebdf9b5947e9d70cd98cfc542c854e7
Nezramov <pok.karpovich@gmail.com>
2022-04-28 14:41:25 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Div from "./divMove";
-import MoveImg from "./clickImg";
-import TextReverse from "./lesson";
-import Calc from "./calc";
-import ShowImg from "./ShowImg";
 
 function App() {
-  return (
-    <>
-      <ShowImg/>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
